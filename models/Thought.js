3c62b8eaae521b48dbe150f7d9cf70bb76522ee2
KristoferVickers <KRISTOFERVICKERS2018@GMAIL.COM>
2024-02-05 15:23:19 -0600

    Adding last nights work on models folder

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+const ReactionSchema = require("./Reaction");
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+      minlength: 1,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [ReactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+const ThoughtModel = model("Thought", ThoughtSchema);
+
+module.exports = ThoughtModel;
--- utils/seed.js
+++ utils/seed.js
@@ -13,3 +13,14 @@ const users = [
 
 console.log(connection);
 
+connection.once("open", async () => {
+    console.log("connected");
+
+    await User.deleteMany({});
+
+    await User.collection.insertMany(users);
+
+    console.table(users);
+    console.info("Seeding complete! 🌱");
+    process.exit(0);
+  });
